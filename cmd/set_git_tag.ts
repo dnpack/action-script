09d6157cc2fc49916378d5ef61fdf7b506c4cf94
Eavid <ganrn@vip.qq.com>
2023-12-07 21:40:59 +0800

    fix: git 本地 commit 长度过段找不到 tag 的问题

--- cmd/set_git_tag.ts
+++ cmd/set_git_tag.ts
@@ -3,82 +3,105 @@ import { execCmdWaitExit } from "./private/exec.ts";
 import * as gitCmd from "./git.ts";
 import path from "node:path";
 
-const gitAction = {
-    async deleteTags(tagName: string, lessThan: string | SemverVersion) {
-        if (typeof lessThan === "string") lessThan = new SemverVersion(lessThan);
-        const tags = await this.matchTags(tagName, lessThan);
-        if (tags.length === 0) return [];
-        await execCmdWaitExit("git", { args: ["tag", "-d", ...tags] });
-        return tags;
-    },
-    /**
-     * @remarks 获取匹配 tagName 的标签
-     * @param lessThan 如果存在，将匹配小这个版本的标签
-     */
-    async matchTags(tagName: string, lessThan?: string | SemverVersion) {
-        const output = await execCmdWaitExit("git", { args: ["tag", "-l", tagName] });
-        if (lessThan && typeof lessThan === "string") lessThan = new SemverVersion(lessThan);
-        return output.split("\n").filter((item) => {
-            if (item.length === 0) return false;
-            if (lessThan) return SemverVersion.compare(item, lessThan) === -1;
-            return true;
-        });
-    },
-};
-
-export interface ExecCmdIfUpdateOpts {
-    /** 如果为 true，在添加标签之前删除所有匹配的 minor 版本。 例如存在 1.2.1 1.2.3， 添加标签之前删除他们*/
-    deletePatch?: boolean;
-}
-/**
- * 如果不存在标签， 则执行命令并添加标签
- *
- * @param options deletePatch: 删除 小于传入的所有 patch 版本
- * @returns 如果存在匹配（无需升级）返回 null. 否则返回 删除的 tag 列表
- */
-export async function updateGitTag(versionStr: string, options: ExecCmdIfUpdateOpts = {}) {
-    const currentVersion = new SemverVersion(versionStr);
-    const { deletePatch } = options;
-    const matchTags = await gitAction.matchTags(versionStr);
-    if (matchTags.length > 0) return null;
-
-    let deleteTags: string[] = [];
-    if (deletePatch) {
-        currentVersion.anyFlag = "*";
-        deleteTags = await gitAction.deleteTags(versionStr.toString(), versionStr);
+export const gitAction = {
+  /**
+   * @remarks 删除匹配的本地标签
+   * @param lessThan 匹配小于指定版本的标签
+   *
+   */
+  async deleteTags(tagName: string, lessThan: string | SemverVersion) {
+    if (typeof lessThan === "string") lessThan = new SemverVersion(lessThan);
+    const tags = await this.matchVersionTags(tagName, lessThan);
+    if (tags.length === 0) return [];
+    await execCmdWaitExit("git", { args: ["tag", "-d", ...tags] });
+    return tags;
+  },
+  /**
+   * @remarks 获取匹配 tagName 的标签
+   * @param lessThan 如果存在，将匹配小这个版本的标签
+   */
+  async matchVersionTags(tagName: string, lessThan?: string | SemverVersion) {
+    new SemverVersion(tagName);
+    const output = await execCmdWaitExit("git", {
+      args: ["tag", "-l", tagName],
+    });
+    if (lessThan && typeof lessThan === "string") {
+      lessThan = new SemverVersion(lessThan);
     }
-    await gitCmd.tag.add(versionStr);
-
-    return deleteTags;
-}
+    return output.split("\n").filter((item) => {
+      if (item.length === 0) return false;
+      if (lessThan) return SemverVersion.compare(item, lessThan) === -1;
+      return true;
+    });
+  },
+  /** @remarks 获取所有的远程标签 */
+  async getRemoteTags() {
+    const output = await execCmdWaitExit("git", {
+      args: ["ls-remote", "--tag"],
+    });
+    const tags: string[] = [];
+    for (const item of output.split("\n")) {
+      if (item.endsWith("^{}") || item.length === 0) continue;
+      const res = item.match(/refs\/tags\/(?<tag>.+)$/)?.groups;
+      if (res) tags.push(res.tag);
+    }
+    return tags;
+  },
+  /** @remarks 匹配远程标签*/
+  async matchRemoteVersionTags(version: string | SemverVersion) {
+    if (typeof version === "string") version = new SemverVersion(version);
+    const tags = await this.getRemoteTags();
+    const filterList: string[] = [];
+    for (const tag of tags) {
+      try {
+        if (version.include(tag)) filterList.push(tag);
+      } catch (error) {}
+    }
+    return filterList;
+  },
+  /**
+   * @remarks 从远程搜索标签, 如果不存在标签, 则添加标签. 如果存在, 跳过添加
+   * @returns 如果成功添加, 则返回 true, 否则返回 false
+   */
+  async setTag(versionTag: string) {
+    const matchTags = await gitAction.matchRemoteVersionTags(new SemverVersion(versionTag));
+    if (matchTags.length > 0) return false;
+    await gitCmd.tag.add(versionTag);
+    return true;
+  },
+};
+type UpdateRes = {
+  dirname: string;
+  isAdded: boolean;
+  version: SemverVersion;
+};
 export const npmPkg = {
-    async readVersion(pkgDir: string) {
-        const filename = path.resolve(pkgDir, "package.json");
-        const json: Record<string, any> = JSON.parse(await Deno.readTextFile(filename));
-        if (typeof json.version !== "string") throw new Error("version 字段不存在");
-        return json.version;
-    },
-    /**
-     * map:  prefix -> dirname
-     * @returns 如果添加了 tag, 则返回 true. 否则返回 false
-     */
-    async updatePkgTags(map: Record<string, string>, options: ExecCmdIfUpdateOpts) {
-        let skin = true;
-        for (const [prefix, dirname] of Object.entries(map)) {
-            const version: string = await npmPkg.readVersion(dirname);
-            const tag = prefix + version;
-            const deletes = await updateGitTag(tag, options);
-            if (deletes === null) {
-                console.log(tag + ": skin");
-                continue;
-            }
-            console.log(tag + ": added");
-            skin = false;
-
-            if (deletes.length) console.log(prefix + ": delete tags[" + deletes.join(", ") + "]");
-        }
-        return skin;
-    },
+  /** 读取 npm 包的版本字段 */
+  async readVersion(pkgDir: string) {
+    const filename = path.resolve(pkgDir, "package.json");
+    const json: Record<string, any> = JSON.parse(await Deno.readTextFile(filename));
+    if (typeof json.version !== "string") throw new Error("不存在 version 字段");
+    return new SemverVersion(json.version);
+  },
+  /**
+   * @param map prefix -> dirname 前缀到 npm 包的目录映射
+   * @remarks 根据 package.json 中的 version 设置标签(如果标签不存在)
+   */
+  async setGitTagsFromPkg(map: Record<string, string>): Promise<Map<string, UpdateRes>> {
+    const res: Map<string, UpdateRes> = new Map();
+    for (const [prefix, dirname] of Object.entries(map)) {
+      const version = await npmPkg.readVersion(dirname);
+      const tag = prefix + version.toString();
+      const isAdded = await gitAction.setTag(tag);
+      res.set(prefix, { dirname: dirname, isAdded, version });
+      if (!isAdded) {
+        console.log(tag + ": skin");
+        continue;
+      }
+      console.log(tag + ": added");
+    }
+    return res;
+  },
 };
 
 export { execCmdWaitExit };
--- lib/version.test.ts
+++ lib/version.test.ts
@@ -2,110 +2,113 @@ import { SemverVersion, VersionBase } from "./version.ts";
 import { assertEquals, assertObjectMatch, assert, assertThrows, assertFalse } from "std/assert/mod.ts";
 const { test } = Deno;
 const basePase = {
-    major: 1,
-    minor: 21,
-    patch: 4,
+  major: 1,
+  minor: 21,
+  patch: 4,
 };
 test("基础版本", function () {
-    const version = new SemverVersion("1.21.4");
-    const versionBase = toVersionBase(version);
-    assertObjectMatch(versionBase, {
-        ...basePase,
-        prefix: "",
-        suffix: "",
-        version: "1.21.4",
-    } as VersionBase as any);
+  const version = new SemverVersion("1.21.4");
+  const versionBase = toVersionBase(version);
+  assertObjectMatch(versionBase, {
+    ...basePase,
+    prefix: "",
+    suffix: "",
+    version: "1.21.4",
+  } as VersionBase as any);
 });
 test("带前缀", function () {
-    const version = new SemverVersion("v1.21.4");
-    const versionBase = toVersionBase(version);
-    assertObjectMatch(versionBase, {
-        ...basePase,
-        prefix: "v",
-        suffix: "",
-        version: "1.21.4",
-        tag: undefined,
-        tagNum: undefined,
-    } as VersionBase as any);
+  const version = new SemverVersion("v1.21.4");
+  const versionBase = toVersionBase(version);
+  assertObjectMatch(versionBase, {
+    ...basePase,
+    prefix: "v",
+    suffix: "",
+    version: "1.21.4",
+    tag: undefined,
+    tagNum: undefined,
+  } as VersionBase as any);
 });
 test("只有tag后缀", function () {
-    const version = new SemverVersion("1.21.4-beta");
-    const versionBase = toVersionBase(version);
-    assertObjectMatch(versionBase, {
-        ...basePase,
-        prefix: "",
-        suffix: "-beta",
-        tag: "beta",
-        tagNum: undefined,
-        version: "1.21.4",
-    } as VersionBase as any);
+  const version = new SemverVersion("1.21.4-beta");
+  const versionBase = toVersionBase(version);
+  assertObjectMatch(versionBase, {
+    ...basePase,
+    prefix: "",
+    suffix: "-beta",
+    tag: "beta",
+    tagNum: undefined,
+    version: "1.21.4",
+  } as VersionBase as any);
 });
 test("只有tagNum后缀", function () {
-    const version = new SemverVersion("1.21.4-3");
-    const versionBase = toVersionBase(version);
-    assertObjectMatch(versionBase, {
-        ...basePase,
-        prefix: "",
-        suffix: "-3",
-        tag: undefined,
-        tagNum: 3,
-        version: "1.21.4",
-    } as VersionBase as any);
+  const version = new SemverVersion("1.21.4-3");
+  const versionBase = toVersionBase(version);
+  assertObjectMatch(versionBase, {
+    ...basePase,
+    prefix: "",
+    suffix: "-3",
+    tag: undefined,
+    tagNum: 3,
+    version: "1.21.4",
+  } as VersionBase as any);
 });
 test("标准解析", function () {
-    const version = new SemverVersion("v1.21.4-beta.9");
-    const versionBase = toVersionBase(version);
-    assertObjectMatch(versionBase, {
-        ...basePase,
-        prefix: "v",
-        suffix: "-beta.9",
-        version: "1.21.4",
-        tag: "beta",
-        tagNum: 9,
-    } as VersionBase as any);
+  const version = new SemverVersion("v1.21.4-beta.9");
+  const versionBase = toVersionBase(version);
+  assertObjectMatch(versionBase, {
+    ...basePase,
+    prefix: "v",
+    suffix: "-beta.9",
+    version: "1.21.4",
+    tag: "beta",
+    tagNum: 9,
+  } as VersionBase as any);
 });
 test("错误的版本号", function () {
-    assertThrows(() => new SemverVersion("1.2.i"));
-    assertThrows(() => new SemverVersion("1.2.2.beta"));
-    assertThrows(() => new SemverVersion("1.-.i"));
+  assertThrows(() => new SemverVersion("1.2.i"));
+  assertThrows(() => new SemverVersion("1.2.2.beta"));
+  assertThrows(() => new SemverVersion("1.-.i"));
 });
 test("比较", function () {
-    assertEquals(SemverVersion.compare("1.2.3", "1.2.2"), 1);
-    assertEquals(SemverVersion.compare("1.2.4", "1.2.4"), 0);
-    assertEquals(SemverVersion.compare("1.5.3", "1.2.43"), 1);
-    assertEquals(SemverVersion.compare("2.1.3", "1.2.43"), 1);
+  assertEquals(SemverVersion.compare("1.2.3", "1.2.2"), 1);
+  assertEquals(SemverVersion.compare("1.2.4", "1.2.4"), 0);
+  assertEquals(SemverVersion.compare("1.5.3", "1.2.43"), 1);
+  assertEquals(SemverVersion.compare("2.1.3", "1.2.43"), 1);
 
-    assertEquals(SemverVersion.compare("1.2.3-beta", "1.2.3"), 1);
-    assertEquals(SemverVersion.compare("1.2.3-1", "1.2.3"), 1);
-    assertEquals(SemverVersion.compare("1.2.3-beta.4", "1.2.3-beta.3"), 1);
+  assertEquals(SemverVersion.compare("1.2.3-beta", "1.2.3"), 1);
+  assertEquals(SemverVersion.compare("1.2.3-1", "1.2.3"), 1);
+  assertEquals(SemverVersion.compare("1.2.3-beta.4", "1.2.3-beta.3"), 1);
 
-    assertEquals(SemverVersion.compare("1.2.3", "1.x.2"), 1);
+  assertEquals(SemverVersion.compare("1.2.3", "1.x.2"), 1);
 });
 test("toString", function () {
-    assertEquals(new SemverVersion("1.2.x").toString(), "1.2.x");
-    assertEquals(new SemverVersion("1.*.x").toString(), "1.x.x");
-    assertEquals(new SemverVersion("*.2.x").toString(), "x.2.x");
+  assertEquals(new SemverVersion("1.2.x").toString(), "1.2.x");
+  assertEquals(new SemverVersion("1.*.x").toString(), "1.x.x");
+  assertEquals(new SemverVersion("*.2.x").toString(), "x.2.x");
 });
 test("include", function () {
-    assert(new SemverVersion("1.2.x").include("1.2.4"));
-    assert(new SemverVersion("1.2.x").include("1.2.x"));
-    assertFalse(new SemverVersion("1.2.x").include("1.3.4"));
-    assertFalse(new SemverVersion("1.2.x").include("2.0.0"));
+  assert(new SemverVersion("1.2.x").include("1.2.4"));
+  assert(new SemverVersion("1.2.x").include("1.2.x"));
+  assertFalse(new SemverVersion("1.2.x").include("1.3.4"));
+  assertFalse(new SemverVersion("1.2.x").include("2.0.0"));
 
-    assert(new SemverVersion("1.x.x").include("1.3.x-beta"));
-    assert(new SemverVersion("1.x.x").include("1.3.3-3"));
-    assertFalse(new SemverVersion("1.x.x").include("2.0.0"));
-    assert(new SemverVersion("x.x.x").include("2.4.5"));
+  assertFalse(new SemverVersion("1.x.x").include("1.3.x-beta"));
+  assertFalse(new SemverVersion("v1.x.x").include("1.x.x"));
+  assert(new SemverVersion("1.x.x-beta.x").include("1.x.x-beta.3"));
+
+  assert(new SemverVersion("1.x.x").include("1.3.3-3"));
+  assertFalse(new SemverVersion("1.x.x").include("2.0.0"));
+  assert(new SemverVersion("x.x.x").include("2.4.5"));
 });
 function toVersionBase(version: SemverVersion): VersionBase {
-    return {
-        major: version.major,
-        minor: version.minor,
-        patch: version.patch,
-        prefix: version.prefix,
-        suffix: version.suffix,
-        version: version.version,
-        tag: version.tag,
-        tagNum: version.tagNum,
-    };
+  return {
+    major: version.major,
+    minor: version.minor,
+    patch: version.patch,
+    prefix: version.prefix,
+    suffix: version.suffix,
+    version: version.version,
+    tag: version.tag,
+    tagNum: version.tagNum,
+  };
 }
