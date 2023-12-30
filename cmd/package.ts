97f3882ff8914272a81acad87bb49226059f0f5e
Eavid <ganrn@vip.qq.com>
2023-12-30 17:07:21 +0800

    feat(cmd/package): getWorkspaceTagMap

--- cmd/package.ts
+++ cmd/package.ts
@@ -1,2 +1,1 @@
-export * from "./package/set_tag.ts";
 export * from "./package/workspace.ts";
--- cmd/package/workspace.ts
+++ cmd/package/workspace.ts
@@ -1,15 +1,17 @@
 import * as yaml from "../../deps/std/yaml.ts";
+import { SemverVersion } from "../../lib.ts";
 import { glob } from "../../deps/glob.ts";
 import path from "node:path";
-
+/** @public */
+export interface PackageJson {
+  /** package.json 所在文件夹 */
+  packageRoot: string;
+  name?: string;
+  version?: string;
+}
 /** @public */
 export interface PnpmWorkspaceSearchRes {
-  success: {
-    /** 子包的绝对路径 */
-    dir: string;
-    /** 子包的 package.json 信息 */
-    pkg: any;
-  }[];
+  success: PackageJson[];
   fail: any[];
   /** 工作区根包的 package.json 信息 */
   rootPkg: Record<string, any>;
@@ -18,21 +20,27 @@ export interface PnpmWorkspaceSearchRes {
  * @public
  * @remarks 查找PNPM工作区目录下的所有包的信息
  */
-export async function findPnpmWorkspacePkgs(dir: string): Promise<PnpmWorkspaceSearchRes> {
+export async function findPnpmWorkspacePkgs(workspaceRoot: string): Promise<PnpmWorkspaceSearchRes> {
   const [rootPkg, packagesGlob] = await Promise.all([
-    readPackageJson(dir),
-    getPnpmWorkspaceDefine(path.resolve(dir, "pnpm-workspace.yaml")),
+    readPackageJson(workspaceRoot),
+    getPnpmWorkspaceDefine(path.resolve(workspaceRoot, "pnpm-workspace.yaml")),
   ]);
   for (let i = 0; i < packagesGlob.length; i++) {
     if (!packagesGlob[i].endsWith("/")) packagesGlob[i] = packagesGlob[i] + "/package.json";
   }
-  const packageFile = await glob(packagesGlob, { root: dir, cwd: dir, nodir: true, absolute: true });
+  const packageFile = await glob(packagesGlob, {
+    root: workspaceRoot,
+    cwd: workspaceRoot,
+    nodir: true,
+    absolute: true,
+  });
   const { success, fail } = await allSettled(
-    packageFile.map(async (filename) => {
+    packageFile.map(async (filename): Promise<PackageJson> => {
       const dir = path.resolve(filename, "..");
       const text = await Deno.readTextFile(filename);
-      return { dir, pkg: JSON.parse(text) };
-    })
+
+      return { ...JSON.parse(text), packageRoot: dir };
+    }),
   );
 
   return { success, fail, rootPkg };
@@ -65,4 +73,32 @@ async function getPnpmWorkspaceDefine(file: string): Promise<string[]> {
   }
 }
 
-// export async function packNpmPkg(dir: string) {}
+/**
+ * @public
+ * @remarks 获取 Pnpm 工作区子包, 返回 标签到包的映射
+ */
+export async function getWorkspaceTagMap(pkgInfoList?: PackageJson[]) {
+  if (!pkgInfoList) {
+    const { success, fail } = await findPnpmWorkspacePkgs(".");
+    if (fail.length) console.log(`查找失败: ${fail.join(", ")}`);
+    pkgInfoList = success;
+  }
+
+  if (pkgInfoList.length === 0) console.log("没有任何升级, 跳过");
+  const tagsMap: Record<string, PackageJson> = {};
+  for (const pkg of pkgInfoList) {
+    const dir = pkg.packageRoot;
+    let name = pkg.name;
+    if (typeof name !== "string") {
+      name = dir.slice(dir.lastIndexOf(path.sep));
+    }
+    if (name.startsWith("@")) name = name.slice(name.indexOf("/") + 1);
+    if (name === "") throw new Error(dir + ": 包名无效");
+
+    const version = new SemverVersion(pkg.version!);
+    version.prefix = name + "/v";
+    const tag = version.toString();
+    tagsMap[tag] = pkg;
+  }
+  return tagsMap;
+}
